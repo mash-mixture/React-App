624d40014437dcf3b7cbe477055c243181f2aec4
mash-mixture <asadorasoo32@gmail.com>
2023-01-24 09:15:11 +0500

    url-fixing

--- src/components/header.js
+++ src/components/header.js
@@ -9,8 +9,8 @@ const header = () => {
             </div>
             <div className="nav-menu">
                 <ul>
-                    <li><Link to={`/`}>Home</Link></li>
-                    <li><Link to={`/about`}>About</Link></li>
+                    <li><Link to={`/React-App/`}>Home</Link></li>
+                    <li><Link to={`/React-App/about`}>About</Link></li>
                 </ul>
             </div>
         </div>
